706c8ecb29be74f431c5a84093a3ce5afed6ac3f
ehdcjf <kdch0823@naver.com>
2021-10-28 02:55:57 +0900

    기능추가: 마이페이지 리스트

---
+++ front/client/component/MyNFT/NFTItem/NFTItem.css.jsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+export const StyledMyNFT = styled.tr`
+  width: 100%;
+  &>td{
+    padding: 10px;
+  }
+  
+  &:hover{
+    background-color: #f7f7f7;
+  } 
+
+  .NFT_info{
+    display: flex;
+  }
+
+  
+  .NFT_img>img{
+      width: 120px;
+      height: 120px;
+      overflow: hidden;
+      padding: 4px;
+    }
+  
+  .NFT_detail{
+    padding-left: 10px;
+    font-size: 14px;
+    flex:1;
+  }
+
+  &>td:nth-child(2),
+  &>td:nth-child(3),
+  &>td:nth-child(4),
+  &>td:nth-child(5){
+    text-align: center;
+  }
+
+
+
+
+`
