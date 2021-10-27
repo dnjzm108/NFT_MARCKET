0010d8d41813c1ba09b71cd854f669154cad7036
GUMUNYEONG <csprout@nate.com>
2021-10-27 14:56:04 +0900

    release - 위치수정 / 대표사진

--- front/client/component/Thumbnail/Thumbnail.css.jsx
+++ front/client/component/Thumbnail/Thumbnail.css.jsx
@@ -1,30 +1,63 @@
 import styled from "styled-components"
+import Slider from "react-slick";
 
 export const StyledThumbnail = styled.div`
 
 .thumbnail{
-    
+    width: auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 4%;
 }
 
 .thumbnail>h3{
-    display: flex;
-    text-align: center;
-    flex-direction: column;
-    padding: 6%;
+    width: 300px;
+    padding: 8%;
 }
 
-.flex_contain{
-    width: 400px;
-    display: flex;
-    align-items: center;
+.thumbnail>p{
+    width: 280px;
+    margin-top: 20%;
+    text-align: center;
+    font-weight: bold;
+    color: #222;
+}
+`
+
+export const Styled_Slide = styled(Slider)`
+
+.slick-list {
+    width: 300px;
+    height: 400px;
+    margin: 0 auto;
+    background-color: #fcd8fc;
+  }
+
+.slick-prev:before, .slick-next:before{
+    font-family: 'slick';
+    font-size: 40px;
+    line-height: 1;
+    opacity: .75;
+    color: #000000;
+    -webkit-font-smoothing: antialiased;
+}
+
+.slick-prev:before{
+    position: relative;
+    left: -20px;
+}
+
+div>button{
+    width: 50px;
+    float: right;
+    border-radius: 30px;
+    border: 1px solid #222;
+    background-color: ${p=> !p.isSelect ? "#222": "white"};
+    color: ${p=> !p.isSelect ?  "white": "#222"};
+    padding: 4px;
+    margin: 6%;
     cursor: pointer;
 }
 
-.pass{
-    margin: 0 auto;
-    width: 60%;
-    display: flex;
-    justify-content: space-evenly;
-    align-items: center;
-}
 `
--- front/client/component/Thumbnail/index.jsx
+++ front/client/component/Thumbnail/index.jsx
@@ -1,84 +1,69 @@
-// import { StyledThumbnail } from './Thumbnail.css'
-// import NTF from '../NFT/index'
+import { StyledThumbnail } from './Thumbnail.css'
+import NTF from '../NFT/index'
 // import { FiChevronLeft,FiChevronRight } from "react-icons/fi";
 // import { GrRadial , GrStatusGoodSmall } from "react-icons/gr";
-// import { useState } from 'react';
-
-// const Thumbnail = ()=>{
-//     const [isPicture, setIsPicture] = useState(true)
-//     const [prevPicture, setPrevPicture] = useState(true)
-//     const [nxtPicture, setNxtPicture] = useState(true)
-    
-//     const handlePass = ()=>{
-//         setIsPicture(!isPicture)
-//     }
-
-//     return(
-//         <StyledThumbnail>
-//             <div className="thumbnail">
-//                 <h3>대표 사진을 선택해주세요</h3>
-//                 <div className="flex_contain">
-//                     <FiChevronLeft size="30" />
-//                     <NTF />
-//                     <FiChevronRight size="30" />
-//                 </div>
-//             </div>
-//             <div className="pass">
-//                 {/* {
-//                     isPicture ? <GrRadial size="10"/> : <GrStatusGoodSmall size="14"/>
-//                 } */}
-//             <GrRadial size="10"/><GrRadial size="10"/><GrRadial size="10"/>
-//             <GrStatusGoodSmall size="14"/><GrRadial size="10"/><GrRadial size="10"/>
-//             <GrRadial size="10"/><GrRadial size="10"/><GrRadial size="10"/>
-//             </div>
-//         </StyledThumbnail>
-//     )
-// }
-
-// export default Thumbnail
-
+import { useState } from 'react';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import React, { Component } from "react";
-import Slider from "react-slick";
+import {Styled_Slide} from "./Thumbnail.css"
 
 
+const Thumbnail = ()=>{
+    const [ isSelect , setIsSelect ] = useState(true);
 
-export default class SimpleSlider extends Component {
-  render() {
+    const handleSelect = ()=>{
+        setIsSelect(!isSelect)
+    }
+
     const settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1
-    };
-    return (
-      <div>
-        <h2> Single Item</h2>
-        <Wrap>
-        <Slider {...settings}>
-          <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
-          </div>
-        </Slider>
-        </Wrap>
-      </div>
-    );
-  }
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    }
+
+
+    return(
+        <StyledThumbnail>
+            <div className="thumbnail">
+                <h3>대표 이미지를 선택해주세요</h3>
+                <div className="flex_contain">
+                    <Styled_Slide {...settings} isSelect={isSelect}>
+                        {
+                            
+                        }
+                        <div>
+                            <button onClick={()=>handleSelect()}>선택</button>
+                            <h3>1</h3>
+                        </div>
+                        <div>
+                            <NTF/>
+                        </div>
+                        <div>
+                            <h3>3</h3>
+                        </div>
+                        <div>
+                            <h3>4</h3>
+                        </div>
+                        <div>
+                            <h3>5</h3>
+                        </div>
+                        <div>
+                            <h3>6</h3>
+                        </div>
+                    </Styled_Slide>
+                </div>
+                <p>* 선택하지 않으면 가장 앞의 사진이 대표사진으로 올라갑니다</p>
+            </div>
+        </StyledThumbnail>
+    )
 }
+
+export default Thumbnail
+
+
+
+
+
