5abcc499ca027c3f46af2b4d5674354ceaf3b3fb
Sangyub <godtttever@naver.com>
2021-10-27 22:28:43 +0900

    나의 프로필화면 조정, alert 추가

--- front/client/component/Profile/index.jsx
+++ front/client/component/Profile/index.jsx
@@ -17,25 +17,25 @@ const Profile = () => {
         <ProfileStyled>
             <div>
                 <h1>나의 프로필</h1>
-                    <img src="/홍준표.png" alt="" />
+                    <img src="/3.jpg" alt="" />
                     <StyleTd/>
                     <div className="seller_title">
                     <h2>네이선드레이크</h2>
                     <h5 className="verified"><VscCheck/>인증 완료</h5>
-                    <h5 className="need_verified"><VscWarning/>인증 필요</h5>
+                    <h5 className="need_verified"><VscWarning/>판매자 인증 필요</h5>
                     </div>
                     <StyleTd/>
                     <h4>지갑 주소</h4>
                     <StyleTd/>
                     <CopyToClipboard text={WalletUrl}>
-                    <td className="URL">0xbf39ac77b62577d4c8e9c16f278b1c05e87d17e5</td>
+                    <td className="URL"onClick={() => alert('주소가 복사되었습니다.')}>0xbf39ac77b62577d4c8e9c16f278b1c05e87d17e5</td>
                     </CopyToClipboard>
                     <StyleTd/>
                     <h4>이메일 주소</h4>
                     <StyleTd/>
                     <h4>godtttever@naver.com</h4>
                     <StyleTd/>
-                    <Button value='판매자 등록신청' url='/' ml={100} color="sky" size='small' />
+                    <Button value='판매자 등록신청' url='/' ml={110} color="sky" size='small' />
             </div>
         </ProfileStyled>
         </>
