44e02141b113393b57747f16559532c946fea553
dnjzm108 <dnjzm108@kakao.com>
2021-11-02 10:52:51 +0900

    axios

--- back/routers/user/user.controller.js
+++ back/routers/user/user.controller.js
@@ -1,12 +1,23 @@
+
 const { query } = require("../../pool")
 
-let test = (req,res) =>{
+let test = async (req,res) =>{
     
     const sql =``
     const result = await query(sql)
 }
 
+let join = async (req,res) =>{
+    let {nickname,wallet,email,picture} = req.body
+    const sql =`insert into user (nickname,wallet,email,picture) values ("${nickname}","${wallet}","${email}","${picture}")`
+    const result = await query(sql)
+
+    console.log(result);
+
+} 
+
 module.exports={
-    test
+    test,
+    join
 }
 
--- front/client/pages/index.jsx
+++ front/client/pages/index.jsx
@@ -1,19 +1,11 @@
 import Head from 'next/head'
 import { useEffect } from 'react'
 import Explore from '../container/explore'
-import caver from '../klaytn/caver'
 
 
 
 const Home = () => {
 
-    useEffect(async()=>{
-       const Caver = caver.klay;
-       console.log(Caver)
-
- 
-
-    },[])
 
     return (
         <>
