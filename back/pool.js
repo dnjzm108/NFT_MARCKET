c48238c86a89bf503366c78cca745c01d29b3fc0
seongjin <dnjzm108@kakao.com>
2021-11-10 01:18:51 +0900

    auction 작업중

--- back/pool.js
+++ back/pool.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: '1234',
+    password: '0000',
     database: 'nft_market',
     multipleStatements: true,
     connectionLimit: 100
--- front/client/pages/nft/[id].jsx
+++ front/client/pages/nft/[id].jsx
@@ -1,38 +1,13 @@
 import Product_Detail_Component from "../../component/Product_Detail"
 
 const Product_Detail = () => {
-    const router = useRouter()
-    const { id } = router.query
-    const [Img,setImg] = useState([])
-
-    useEffect(async () => {
-        if(id!=undefined){
-            let data = {
-                    product_no : id
-                }
-                let result = await axios.post(`${url}/product/product_detail`,data)
-                console.log(result.data.img);
-                result.data.img.map((v)=>{
-                     setImg( v.img)  
-                     console.log(v.img);
-                    })
-                }
-    }, [id])
-
-const Product_Detail = () => {
    
     return (
         <>
-        {id 
-        ?   <Product_Detail_Component Img={Img} />
-        : <span>로딩중</span>
-        }
+        <Product_Detail_Component />
         </>
     )
 }
-
-
-
 
 export default Product_Detail
 
